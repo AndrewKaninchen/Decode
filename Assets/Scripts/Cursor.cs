dc8586f17c7708ca0f2d94780c65c8a991e16472
André Luiz de Andrade Coelho <andre.luiz.coelho@outlook.com>
2019-10-05 16:25:14 -0300

    Integrando Tilemap com o jogo em si de forma gambiarrenta

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Decode;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Cursor : MonoBehaviour
+{
+    public Pawn playerPawn;
+    //public BoardView boardView;
+    
+    private async void Update()
+    {
+        if (!GameController.Instance.HasStarted || GameController.Instance.HasEnded)
+            return;
+        
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Tile hoveredTile = null;
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var hitObject = hit.collider.gameObject;
+            var tile = hitObject.GetComponentInParent<Tile>();
+            var pawn = hitObject.GetComponent<Pawn>();
+            if (tile)
+            {
+                var lastHoveredTile = hoveredTile;
+                hoveredTile = hit.collider.GetComponentInParent<Tile>();
+
+                if (lastHoveredTile != hoveredTile)
+                {
+                    //resetar os gráfico  
+                }
+
+                if (hoveredTile)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        await playerPawn.Move(hoveredTile.position);
+                        var p = GameController.Instance.Players[1] as HumanPlayer;
+                        p.EndTurn();
+                    }
+                    else
+                    {
+                        //setar os gráfico
+                    }
+                }
+            }
+            else if (pawn)
+            {
+                if(Input.GetMouseButtonDown(0))
+                    playerPawn.Attack(pawn);
+            }
+        }
+        
+        
+    }
+}
---
+++ Assets/Scripts/Editor/TestBrush.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+ 
+[CustomGridBrush(false, false, false, "Test Brush")]
+public class TestBrush : GridBrush
+{
+    private Tilemap targetTilemap;
+
+    public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, Vector3Int pickStart)
+    {
+        base.Pick(gridLayout, brushTarget, position, pickStart);
+        var prefabTile = cells[0].tile as PrefabTile;
+        if (prefabTile == null) return;
+            
+        var tilemapName = prefabTile.tilemapName;
+        var tilemaps = GameObject.FindObjectsOfType<Tilemap>().ToList();
+        targetTilemap = tilemaps.Find(tilemap => tilemap.name.Equals(tilemapName));
+        if (targetTilemap == null) return;
+
+        GridPaintingState.scenePaintTarget = targetTilemap.gameObject;
+//        
+//        var lastSelected = Selection.activeObject;
+//        Selection.activeObject = targetTilemap.gameObject;
+//        Selection.selectionChanged();
+//        Selection.activeObject = lastSelected;
+    }
+
+//    public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+//    {
+//        if (brushTarget != null)
+//        {
+//            Undo.RegisterCompleteObjectUndo(this.targetTilemap, string.Empty);
+//            base.Paint(gridLayout, brushTarget, position);
+//        }
+//    }
+//
+//    public override void BoxFill(GridLayout gridLayout, GameObject brushTarget, BoundsInt position)
+//    {
+//        if (brushTarget != null)
+//        {
+//            Undo.RegisterCompleteObjectUndo(this.targetTilemap, string.Empty);
+//            base.BoxFill(gridLayout, brushTarget, position);
+//        }
+//    }
+//
+//    public override void FloodFill(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+//    {
+//        if (brushTarget != null)
+//        {
+//            Undo.RegisterCompleteObjectUndo(this.targetTilemap, string.Empty);
+//            base.FloodFill(gridLayout, brushTarget, position);
+//        }
+//    }
+//
+//    public override void Erase(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+//    {
+//        if (brushTarget != null)
+//        {
+//            Undo.RegisterCompleteObjectUndo(this.targetTilemap, string.Empty);
+//            base.Erase(gridLayout, brushTarget, position);
+//        }
+//    }
+//
+//   
+//    public override void BoxErase(GridLayout gridLayout, GameObject brushTarget, BoundsInt position)
+//    {
+//        if (brushTarget != null)
+//        {
+//            Undo.RegisterCompleteObjectUndo(this.targetTilemap, string.Empty);
+//            base.BoxErase(gridLayout, brushTarget, position);
+//        }
+//    }
+}
